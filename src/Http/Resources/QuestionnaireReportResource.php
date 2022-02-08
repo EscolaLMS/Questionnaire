ad3b966c11299ef1c6d6c27b9a1e1a2ca11aafa0
KD <do>
2022-02-08 12:15:38 +0100

    STAR-57 - swagger fix

--- src/Http/Resources/QuestionnaireReportResource.php
+++ src/Http/Resources/QuestionnaireReportResource.php
@@ -4,6 +4,36 @@ namespace EscolaLms\Questionnaire\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="QuestionnaireReportResource",
+ *     @OA\Property(
+ *          property="sum_rate",
+ *          type="string",
+ *          description="sum of rates for this question"
+ *     ),
+ *     @OA\Property(
+ *         property="count_answers",
+ *         type="integer",
+ *         description="count all answers for this question"
+ *     ),
+ *     @OA\Property(
+ *          property="avg_rate",
+ *          type="string",
+ *          description="average rate"
+ *     ),
+ *     @OA\Property(
+ *         property="question_id",
+ *         type="integer",
+ *         description="question identifier"
+ *     ),
+ *     @OA\Property(
+ *          property="title",
+ *          type="string",
+ *          description="question title"
+ *     ),
+ * )
+ */
 class QuestionnaireReportResource extends JsonResource
 {
     public function toArray($request)
