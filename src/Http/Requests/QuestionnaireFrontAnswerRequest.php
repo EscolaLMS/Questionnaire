22e8e096a5bf7e6b6a5808f3a90976640a4fa301
KrzysztofDziedziechEscolasoft <96292232+KrzysztofDziedziechEscolasoft@users.noreply.github.com>
2022-02-09 12:15:19 +0100

    Update src/Http/Requests/QuestionnaireFrontAnswerRequest.php
    Co-authored-by: Paweł Chołaj <pawel.cholaj90@gmail.com>

--- src/Http/Requests/QuestionnaireFrontAnswerRequest.php
+++ src/Http/Requests/QuestionnaireFrontAnswerRequest.php
@@ -14,19 +14,24 @@ use Illuminate\Validation\Rule;
  *     schema="QuestionnaireAnswerRequest",
  *     @OA\Property(
  *          property="answers",
- *          type="array",
  *          description="answers for questionnaire"
+ *          @OA\Schema(
+ *              type="array",
+ *              @OA\Items(
+ *                  type="object",
+ *                  @OA\Property(
+ *                      property="question_id",
+ *                      type="integer",
+ *                      description="question identified by id",
+ *                  ),
+ *                  @OA\Property(
+ *                      property="rate",
+ *                      type="integer",
+ *                      description="rate from 1 to 5",
+ *                  ),
+ *              )
+ *          )
  *     ),
- *     @OA\Property(
- *         property="answers.*.question_id",
- *         type="integer",
- *         description="question identified by id"
- *     ),
- *     @OA\Property(
- *          property="answers.*.rate",
- *          type="integer",
- *          description="rate from 1 to 5"
- *     )
  * )
  */
 class QuestionnaireFrontAnswerRequest extends FormRequest
