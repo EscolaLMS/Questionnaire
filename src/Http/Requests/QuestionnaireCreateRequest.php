a0b2a8a5e6e3c97411efc35c880fd3d63c891835
KD <do>
2022-02-28 15:19:03 +0100

    fix

--- src/Http/Requests/QuestionnaireCreateRequest.php
+++ src/Http/Requests/QuestionnaireCreateRequest.php
@@ -3,8 +3,10 @@
 namespace EscolaLms\Questionnaire\Http\Requests;
 
 use EscolaLms\Questionnaire\Models\Questionnaire;
+use EscolaLms\Questionnaire\Models\QuestionnaireModelType;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
 
 class QuestionnaireCreateRequest extends FormRequest
 {
@@ -16,11 +18,11 @@ class QuestionnaireCreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'string|required',
+            'title' => ['string', 'required'],
             'active' => 'boolean',
             'models' => ['sometimes', 'array'],
             'models.*' => ['sometimes', 'array'],
-            'models.*.model_type_id' => ['integer'],
+            'models.*.model_type_id' => ['integer', Rule::exists(QuestionnaireModelType::class, 'id')],
             'models.*.model_id' => ['integer'],
         ];
     }
