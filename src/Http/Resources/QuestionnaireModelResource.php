4af26f98498d159b6c2bf2b2765c03846ceee079
KD <do>
2022-02-28 14:02:56 +0100

    fix

--- src/Http/Resources/QuestionnaireModelResource.php
+++ src/Http/Resources/QuestionnaireModelResource.php
@@ -14,13 +14,15 @@ class QuestionnaireModelResource extends JsonResource
 
     public function toArray($request)
     {
-        $model = new $this->modelableType->model_class();
+        //$model = new $this->modelableType->model_class();
 
         return [
             'id' => $this->id,
-            'model_type' => $this->modelableType->model_class,
+            'model_type_title' => $this->modelableType->title,
+            'model_type_class' => $this->modelableType->model_class,
+            'model_type_id' => $this->model_type_id,
             'model_id' => $this->model_id,
-            'model' => $model::find($this->model_id),
+            //'model' => $model::find($this->model_id),
         ];
     }
 }
