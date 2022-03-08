14bf257d440495abd78badaad4f4a3a1a165ac7f
KD <do>
2022-03-08 12:24:57 +0100

    STAR-438

--- src/Models/Question.php
+++ src/Models/Question.php
@@ -42,6 +42,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  *          type="string",
  *          description="type: rate, text"
  *     ),
+ *     @OA\Property(
+ *          property="questions",
+ *          type="array",
+ *          @OA\Items(ref="#/components/schemas/QuestionAnswer")
+ *     ),
  * )
  *
  * @property integer $id
