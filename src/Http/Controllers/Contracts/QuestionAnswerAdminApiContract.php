4d20557e064257f3d29fbeb166a9d1cba00eacff
KD <do>
2022-03-10 17:51:46 +0100

    fix swagger

--- src/Http/Controllers/Contracts/QuestionAnswerAdminApiContract.php
+++ src/Http/Controllers/Contracts/QuestionAnswerAdminApiContract.php
@@ -30,6 +30,7 @@ interface QuestionAnswerAdminApiContract
      *         @OA\Schema(
      *            type="integer",
      *         ),
+     *         in="query",
      *         required=false
      *     ),
      *     @OA\Parameter(
@@ -38,6 +39,7 @@ interface QuestionAnswerAdminApiContract
      *         @OA\Schema(
      *            type="integer",
      *         ),
+     *         in="query",
      *         required=false
      *     ),
      *     @OA\Response(
